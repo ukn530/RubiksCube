288501a3c9c62fbd6b17083596e42d669d9e0bc7
Tatsuya Uchida <ukn530@gmail.com>
2025-05-09 15:23:24 +0900

    trigger enter

--- Assets/Scripts/GrabberController.cs
+++ Assets/Scripts/GrabberController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
@@ -7,24 +8,29 @@ public class GrabberController : MonoBehaviour
     Vector3 _forward;
     bool _isRotating;
     public bool IsRotating => _isRotating;
+    Vector3 _basePosition;
 
     void Start()
     {
         _forward = transform.right;
+        _basePosition = transform.position;
+        transform.position = _basePosition * 100;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("Grabber"))
+        if (other.gameObject.CompareTag("Cell"))
         {
             for (int i = 0; i < _grabbedObjects.Length; i++)
             {
                 if (_grabbedObjects[i] == null)
                 {
                     _grabbedObjects[i] = other.gameObject;
+                    _grabbedObjects[i].transform.parent = transform;
                     break;
                 }
             }
+
         }
     }
 
@@ -32,24 +38,24 @@ public class GrabberController : MonoBehaviour
     {
         if (_isRotating) return;
         _isRotating = true;
+        transform.position = _basePosition;
+        StartCoroutine(RotateFaceCoroutine());
+    }
 
-        for (int i = 0; i < _grabbedObjects.Length; i++)
-        {
-            if (_grabbedObjects[i] != null)
-            {
-                _grabbedObjects[i].transform.parent = transform;
-            }
-        }
-
-        transform.DOBlendableLocalRotateBy(_forward * 90, 0.2f).SetEase(Ease.OutCubic).OnComplete(() =>
+    IEnumerator RotateFaceCoroutine()
+    {
+        yield return null;
+        transform.DOBlendableLocalRotateBy(_forward * 90, 0.5f).SetEase(Ease.OutCubic).OnComplete(() =>
         {
             for (int i = 0; i < _grabbedObjects.Length; i++)
             {
                 if (_grabbedObjects[i] != null)
                 {
                     _grabbedObjects[i].transform.parent = transform.parent;
+                    _grabbedObjects[i] = null;
                 }
             }
+            transform.position = _basePosition * 100;
             _isRotating = false;
         });
     }
