a0c9cc05a9db979938c0840e83a20a5e0d998b46
Tatsuya Uchida <ukn530@gmail.com>
2025-05-11 12:20:05 +0900

    add smooth rotation

--- Assets/Scripts/GrabberController.cs
+++ Assets/Scripts/GrabberController.cs
@@ -1,18 +1,62 @@
-using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class GrabberController : MonoBehaviour
+public class GrabberController : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler, IPointerDownHandler, IPointerUpHandler
 {
     List<GameObject> _grabbedObjects = new List<GameObject>();
     Vector3 _forward;
     bool _isRotating;
     public bool IsRotating => _isRotating;
 
+    bool _isClicking;
+    bool _isDragging;
+
     void Start()
     {
         _forward = transform.right;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} がクリックされたよ！");
+        if (!_isDragging)
+        {
+            RotateFace();
+        }
+        _isClicking = false;
+        _isDragging = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} にマウスが乗ったよ！");
+        // PreRotateFace();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} からマウスが離れたよ！");
+        // ResetRotation();
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} にマウスが移動したよ！");
+        // PreRotateFace();
+        _isDragging = _isClicking;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} がクリックされたよ！");
+        _isClicking = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        print($"オブジェクト {name} のクリックが離れたよ！");
     }
 
     void OnTriggerEnter(Collider other)
@@ -28,6 +72,50 @@ public class GrabberController : MonoBehaviour
         {
             _grabbedObjects.Remove(other.gameObject);
         }
+    }
+
+    void PreRotateFace()
+    {
+        foreach (var grabbedObject in _grabbedObjects)
+        {
+            if (grabbedObject != null)
+            {
+                grabbedObject.transform.parent = transform;
+            }
+        }
+
+        transform.DOBlendableLocalRotateBy(_forward * 5, 0.3f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            foreach (var grabbedObject in _grabbedObjects)
+            {
+                if (grabbedObject != null)
+                {
+                    grabbedObject.transform.parent = transform.parent;
+                }
+            }
+        });
+    }
+
+    void ResetRotation()
+    {
+        foreach (var grabbedObject in _grabbedObjects)
+        {
+            if (grabbedObject != null)
+            {
+                grabbedObject.transform.parent = transform;
+            }
+        }
+
+        transform.DOBlendableLocalRotateBy(_forward * -5, 0.3f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            foreach (var grabbedObject in _grabbedObjects)
+            {
+                if (grabbedObject != null)
+                {
+                    grabbedObject.transform.parent = transform.parent;
+                }
+            }
+        });
     }
 
     public void RotateFace()
--- Assets/Scripts/ViewController.cs
+++ Assets/Scripts/ViewController.cs
@@ -1,25 +1,35 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class ViewController : MonoBehaviour
 {
-    public float rotationSpeed = 100f;
-    bool _enabled = true;
+    [SerializeField] float _rotationSpeed = 100f;
+    [SerializeField] float _inertiaDamping = 10f; // 慣性の減衰速度
+    Vector2 _lastRotationInput = Vector2.zero; // 現在の回転速度
+    Vector2 _currentRotationVelocity = Vector2.zero; // 現在の回転速度
+    private bool _enabled = true;
     public bool Enabled => _enabled;
 
     void Update()
     {
-        if (!_enabled)
-            return;
 
-        if (Mouse.current.leftButton.isPressed)
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonUp(0))
         {
-            var rotationInput = Mouse.current.delta.ReadValue();
-            float rotationX = rotationInput.y * rotationSpeed * Time.deltaTime;
-            float rotationY = -rotationInput.x * rotationSpeed * Time.deltaTime;
-
-            transform.Rotate(Vector3.right, rotationX, Space.World);
-            transform.Rotate(Vector3.up, rotationY, Space.World);
+            var rotationInput = Input.mousePositionDelta;
+            if (rotationInput.sqrMagnitude > 0.01f || _lastRotationInput.sqrMagnitude < 0.01f)
+            {
+                _currentRotationVelocity = rotationInput * _rotationSpeed;
+            }
+            _lastRotationInput = rotationInput;
         }
+        else
+        {
+            _currentRotationVelocity = Vector2.Lerp(_currentRotationVelocity, Vector2.zero, _inertiaDamping * Time.deltaTime);
+        }
+
+        float rotationX = _currentRotationVelocity.y * Time.deltaTime;
+        float rotationY = -_currentRotationVelocity.x * Time.deltaTime;
+
+        transform.Rotate(Vector3.right, rotationX, Space.World);
+        transform.Rotate(Vector3.up, rotationY, Space.World);
     }
 }
